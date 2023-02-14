d01dbd682e6c5e081e4a9712ce42312b18f06fe3
Akash N O <akash@arminotech.in>
2023-02-14 16:35:30 +0530

    addeed base url to aws

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://chat-express-api.herokuapp.com"
-// const baseUrl = "http://localhost:3001"
+// const baseUrl = "https://chat-express-api.herokuapp.com"
+const baseUrl = "http://13.113.220.151:8080/"
 module.exports={ baseUrl }
